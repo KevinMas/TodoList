564b6205811fd31f703465b6c5abf029f74fa223
KevinMas <maskevin@gmail.com>
2020-02-26 17:30:55 +0900

    [DEV]　単体テストの一つ（例として）

--- app/src/test/java/com/example/todolist/TodoDatabaseTest.kt
+++ app/src/test/java/com/example/todolist/TodoDatabaseTest.kt
@@ -3,12 +3,9 @@ package com.example.todolist
 import android.content.Context
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.example.todolist.data.database.TodoDao
 import com.example.todolist.data.database.TodoDatabase
 import com.example.todolist.data.model.Todo
-import kotlinx.coroutines.Dispatchers.Unconfined
-import kotlinx.coroutines.runBlocking
 import org.junit.After
 import org.junit.Assert.assertTrue
 import org.junit.Before
@@ -24,12 +21,14 @@ import java.io.IOException
 class TodoDatabaseTest {
     private lateinit var todoDao: TodoDao
     private lateinit var db: TodoDatabase
-
+    lateinit var context: Context
     @Before
     fun createDb() {
-        val context = ApplicationProvider.getApplicationContext<Context>()
-        db = Room.inMemoryDatabaseBuilder(context, TodoDatabase::class.java).build()
-        todoDao = db.noteDao()
+        context = ApplicationProvider.getApplicationContext<Context>()
+        db = Room.inMemoryDatabaseBuilder(context, TodoDatabase::class.java)
+            .allowMainThreadQueries()
+            .build()
+        todoDao = db.todoDao()
     }
 
     @After
@@ -42,10 +41,14 @@ class TodoDatabaseTest {
     @Throws(Exception::class)
     fun writeUserAndReadInList() {
         val todo = Todo(1, "todo text", false)
-        runBlocking{
-            todoDao.insertTodo(todo)
-            val todoList = todoDao.getAllTodo().value
-            assertTrue(todoList!!.contains(todo))
+        val inputCount = todoDao.insertTodo(todo)
+        val todoList = todoDao.getAllTodo()
+
+        assertTrue(inputCount == 1.toLong())
+
+        todoList.observeForever { todos ->
+            assertTrue(todos.contains(todo))
         }
+
     }
 }
